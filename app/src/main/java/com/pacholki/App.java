7bb4a19de3ff210d10c7c4cb40d9145b97ba0a96
korneltlaczala <kornel.tlaczala@gmail.com>
2023-12-28 02:34:30 +0100

    add Main.java class to fix error while runnig app.jar

--- app/src/main/java/com/pacholki/App.java
+++ app/src/main/java/com/pacholki/App.java
@@ -38,7 +38,7 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+    // public static void main(String[] args) {
+    //     launch();
+    // }
 }
---
+++ app/src/main/java/com/pacholki/Main.java
@@ -0,0 +1,7 @@
+package com.pacholki;
+
+public class Main {
+    public static void main(final String[] args) {
+        App.launch(App.class, args);
+    }
+}
