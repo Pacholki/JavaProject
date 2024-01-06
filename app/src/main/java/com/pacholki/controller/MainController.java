3a52b0057e9c014a45726688b39c16a5822e09f4
wojcikbart <bartek03w@gmail.com>
2024-01-06 22:19:04 +0100

    preparing leaguetable v2

--- app/src/main/java/com/pacholki/controller/MainController.java
+++ app/src/main/java/com/pacholki/controller/MainController.java
@@ -115,11 +115,6 @@ public class MainController extends Controller implements Initializable {
     public void updatePane(Entity entity) {
         Competition competition = (Competition) entity;
         teamButtonContainer.getChildren().setAll(generateTeamButtons(competition));
-        int gameweek = 10;
-        for(Team team : competition.getTeams()){
-            System.out.println(team);
-            System.out.println(team.getGamesWon(gameweek));
-        }
     }
 
     public List<MFXButton> generateTeamButtons(Competition competition) {
