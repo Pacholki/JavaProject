44ec3f0c734d283c05df2152e6b7bcb9bf5d9ac4
korneltlaczala <kornel.tlaczala@gmail.com>
2024-01-06 23:09:45 +0100

    league pane pain

--- app/src/main/java/com/pacholki/controller/LeagueController.java
+++ app/src/main/java/com/pacholki/controller/LeagueController.java
@@ -1,8 +1,27 @@
 package com.pacholki.controller;
 
-import com.pacholki.entity.Entity;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LeagueController extends Controller {
+import com.pacholki.entity.Entity;
+import com.pacholki.pane.LeaguePane;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class LeagueController extends Controller implements Initializable {
+
+    private LeaguePane leaguePane;
+
+    @FXML
+    private Label leagueNameLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        leaguePane = new LeaguePane(this);
+        leagueNameLabel.setText("dupadupa");
+    }
 
     @Override
     public void updatePane(Entity entity) {
--- app/src/main/java/com/pacholki/pane/LeaguePane.java
+++ app/src/main/java/com/pacholki/pane/LeaguePane.java
@@ -1,5 +1,12 @@
 package com.pacholki.pane;
 
+import com.pacholki.controller.Controller;
+
 public class LeaguePane {
+
+    private Controller controller;
     
+    public LeaguePane(Controller controller) {
+        this.controller = controller;
+    }
 }
