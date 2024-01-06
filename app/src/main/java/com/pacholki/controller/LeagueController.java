96b00d6b0dafac1e1889df2047b1583b1bae3653
wojcikbart <bartek03w@gmail.com>
2024-01-07 00:11:29 +0100

    league table generating columns

--- app/src/main/java/com/pacholki/controller/LeagueController.java
+++ app/src/main/java/com/pacholki/controller/LeagueController.java
@@ -1,6 +1,8 @@
 package com.pacholki.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import com.pacholki.entity.Entity;
@@ -10,6 +12,7 @@ import com.pacholki.pane.MainPane;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class LeagueController extends Controller implements Initializable {
@@ -19,14 +22,25 @@ public class LeagueController extends Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         leaguePane = new LeaguePane(this);
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        leagueTable.getColumns().clear();
+        int gameweek = 13;
+        generateTable();
     }
 
+    public void generateTable() {
+        leagueTable.getColumns().setAll(generateTableColumns());
+
+    }
+
+    public List<TableColumn> generateTableColumns() {
+        List<String> colNames = List.of("Team name", "Played", "Won", "Drawn", "Lost", "GF", "GA", "GD", "Points");
+        List<TableColumn> columns = new ArrayList<>();
+        for(String colName : colNames) {
+            TableColumn column = new TableColumn();
+            column.setText(colName);
+            columns.add(column);
+        }
+        return columns;
+    }
     @Override
     public void updatePane(Entity entity) {
         System.out.println("this league pane should now be updated");
