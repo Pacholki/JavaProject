8e18a189dd6015023bc0ac9b02e44f0944015dc9
korneltlaczala <kornel.tlaczala@gmail.com>
2024-01-18 20:56:51 +0100

    add new pages for team

--- app/src/main/java/com/pacholki/controller/InfoPageController.java
+++ app/src/main/java/com/pacholki/controller/InfoPageController.java
@@ -2,7 +2,7 @@ package com.pacholki.controller;
 
 import com.pacholki.entity.Entity;
 
-public class PlayerPageController extends Controller{
+public class InfoPageController extends Controller{
 
     @Override
     public void updatePane(Entity entity) {
