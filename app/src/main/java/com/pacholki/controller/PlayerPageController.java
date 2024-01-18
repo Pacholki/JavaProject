c91d64d3fe5ebee857a8e9531bb35af1e57489a2
wojcikbart <bartek03w@gmail.com>
2024-01-18 21:15:25 +0100

    add playersTab functionality

--- app/src/main/java/com/pacholki/controller/PlayerPageController.java
+++ app/src/main/java/com/pacholki/controller/PlayerPageController.java
@@ -1,8 +1,24 @@
 package com.pacholki.controller;
 
 import com.pacholki.entity.Entity;
+import com.pacholki.entity.Team;
+import com.pacholki.pane.TeamPane;
+import com.pacholki.util.Tools;
+import javafx.fxml.Initializable;
 
-public class PlayerPageController extends Controller{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlayerPageController extends Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        generatePlayersList(teamPane.getCurrentTeam());
+    }
+
+    public void generatePlayersList(Team team){
+        Tools.display(team.getPlayers());
+    }
 
     @Override
     public void updatePane(Entity entity) {
