ee4ff3d2cf95dec1ac36b0ea1a1c37fc459d1e3a
korneltlaczala <kornel.tlaczala@gmail.com>
2024-01-20 18:43:05 +0100

    suppress music player unused warning

--- app/src/main/java/com/pacholki/App.java
+++ app/src/main/java/com/pacholki/App.java
@@ -14,6 +14,7 @@ import com.pacholki.util.MusicPlayer;
 public class App extends Application {
 
     private static Scene scene;
+    @SuppressWarnings("unused")
     private static MusicPlayer musicPlayer;
 
     @Override
