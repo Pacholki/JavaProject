7cfba5606e8f97eaffecf7967796ea43f30f778f
korneltlaczala <kornel.tlaczala@gmail.com>
2024-01-21 00:16:13 +0100

    remove unnecessary imports

--- app/src/main/java/com/pacholki/entity/Competition.java
+++ app/src/main/java/com/pacholki/entity/Competition.java
@@ -2,7 +2,6 @@ package com.pacholki.entity;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
