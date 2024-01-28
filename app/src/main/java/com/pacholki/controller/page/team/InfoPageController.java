b34906fa163a6a4666950f0e0cb5455cb2698f73
wojcikbart <bartek03w@gmail.com>
2024-01-28 20:27:09 +0100

    infoPage upgrade

--- app/src/main/java/com/pacholki/controller/page/team/InfoPageController.java
+++ app/src/main/java/com/pacholki/controller/page/team/InfoPageController.java
@@ -6,10 +6,13 @@ import java.util.ResourceBundle;
 
 import com.pacholki.controller.Controller;
 import com.pacholki.entity.Entity;
+import com.pacholki.entity.Team;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import static com.pacholki.util.Tools.display;
 
 public class InfoPageController extends Controller {
 
@@ -17,11 +20,28 @@ public class InfoPageController extends Controller {
     private ImageView teamImage;
     @FXML
     private Label teamNameText;
+    @FXML
+    private Label leagueText;
+    @FXML
+    private Label pointsText;
+    @FXML
+    private Label bilansText;
+    @FXML
+    private Label playersCountText;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setImage(teamImage);
-        teamNameText.setText(competitionPane.getCurrentTeam().getName());
+        Integer max_gmw = competitionPane.getCurrentCompetition().getTotalGameweeks();
+        Team team = competitionPane.getCurrentTeam();
+        teamNameText.setText("Team name: " + team.getName());
+        leagueText.setText("League name: " + competitionPane.getCurrentCompetition());
+        pointsText.setText("Points this season: " + team.getPoints(max_gmw) + "  Place: " + team.getTeamPlace(competitionPane.getCurrentCompetition(), team));
+        bilansText.setText("W: " + team.getGamesWon(max_gmw) +
+                "    D: " + team.getGamesDrawn(max_gmw) +
+                "    L: " + team.getGamesLost(max_gmw));
+        playersCountText.setText("Number of players: " + team.getPlayers().size());
+        display(competitionPane.getCurrentCompetition().getTeamsSorted());
     }
 
     private void setImage(ImageView image) {
